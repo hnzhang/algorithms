dff052aab14837b60755af4309c6018499cf47d2
unknown <hzhang@BUR1-D1035505.eac.ad.ea.com>
2015-11-12 15:12:52 -0800

    graph read, BFS

--- graph.cpp
+++ graph.cpp
@@ -1,7 +1,10 @@
 //Graph data structuree
 #include<iostream>
+#include<fstream>
+#include<sstream>
 #include<vector>
 #include<queue>
+#include<string>
 
 class Graph
 {
@@ -16,26 +19,100 @@ public:
 	
 	void readGraph(std::istream& reader)
 	{
-		while( !reader.eof())
+        char buffer[256];
+        bool foundDataSection = false;
+        while (reader.good())
+        {
+            reader.getline(buffer, 256);
+            if (buffer[0] == '#' && buffer[1] == 'd' && buffer[2] == 'a' && buffer[3] == 't' && buffer[4] == 'a')
+            {
+                foundDataSection = true;
+                break;
+            }
+        }
+        if (!foundDataSection)
+            return;
+        initGraph(reader);
+        if (mEdges.size() == 0) 
+            return;
+		while( reader.good())
 		{
+            reader.getline(buffer, 256);
+            std::stringstream strStream(buffer);
 			int x, y, weight;
-			reader >> x >> y >> weight ;
+            strStream >> x >> y >> weight;
 			insertEdge(x, y, weight, mDirected);
 		}
 	}
+
+    void printGraph()
+    {
+        size_t vCount = 0;
+        for (auto v : this->mEdges)
+        {
+            std::cout << "Edges for Vertex[" << vCount << "]";
+            while (v != nullptr)
+            {
+                std::cout << "-->" << v->y;
+                v = v->next;
+            }
+            std::cout << " ||" << std::endl;
+            vCount++;
+        }
+    }
+
+    void BFS(int start)// can start from any vertex
+    {
+        if (mEdges.size() == 0) return;
+        std::vector<int> discovered(mEdges.size(), false);
+        std::vector<int> processed(mEdges.size(), false);
+
+        std::queue<int> q;
+        q.push(start);
+        while (!q.empty())
+        {
+            int v = q.front();
+            q.pop();
+            discovered[v] = true;
+            //processVertexEarlier(v);
+            Edgenode* edge = mEdges[v];
+            while (edge != nullptr)
+            {
+                int y = edge->y;
+                //edge v-->y;
+                if (processed[y] == false || mDirected)
+                {
+                    processEdge(v, y);
+                }
+                if (discovered[y] == false)
+                {
+                    q.push(y);
+                    discovered[y] = true;
+                    mBFSParents[y] = v;
+                }
+                edge = edge->next;
+            }
+        }
+    }
 
 private:
 	void initGraph(std::istream& reader)
 	{
 		//get number of vertices from stream
-		int numOfVertices;
+		int numOfVertices = -1;
 		bool directed;
-		reader >> numOfVertices >> directed;
-		mDirected = directed;
-		mEdges.resize(numOfVertices);
-		mDegree.resize(numOfVertices);
-		mBFSParents.resize(numOfVertices);
-		mDFSParents.resize(numOfVertices);
+        char buffer[256];
+        reader.getline(buffer, 256);
+        std::stringstream strStream(buffer);
+        strStream >> numOfVertices >> directed;
+        if (numOfVertices > 0)
+        {
+            mDirected = directed;
+            mEdges.resize(numOfVertices);
+            mDegree.resize(numOfVertices);
+            mBFSParents.resize(numOfVertices);
+            mDFSParents.resize(numOfVertices);
+        }
 	}
 
 	void insertEdge(int x, int y, int weight, bool directed)
@@ -55,52 +132,26 @@ private:
 	}
 	//=====================================================
 	//Breadth first search and helpers
-	void BFS(int start)// can start from any vertex
-	{
-		if(mEdges.size() == 0) return;
-		std::vector<int> discovered(mEdges.size(), false);
-		std::vector<int> processed(mEdges.size(), false );
+	
 
-		std::queue<int> q;
-        q.push(start);
-		while( !q.empty() )
-		{
-            int v = q.front();
-			discovered[v]= true;
-			//processVertexEarlier(v);
-			Edgenode* edge = mEdges[v];
-			while(edge != nullptr)
-			{
-				int y = edge->y;
-				//edge v-->y;
-				if(processed[y] == false || mDirected)
-				{
-					//processEdge(v, y);
-				}
-				if(discovered[y] == false)
-				{
-					q.push(y);
-					discovered[y] = true;
-					mBFSParents[y] = v;
-				}
-				edge = edge->next;
-			}
-		}
-	}
-
-	void processEdge()
+	void processEdge(int start, int end)
 	{
+        std::cout << "Process edge[ " << start << "," << end << " ]" << std::endl;
 	}
 
 	void processVertexEarlier()
 	{
 	}
 
+    void processVertexLater()
+    {
+
+    }
+
 	void DFS(int start)
 	{
 		//if(mEdges.size() == 0 ) return;
 		//std::stack<int> s;
-
 	}
 
 	std::vector<Edgenode*> mEdges;
@@ -115,5 +166,14 @@ private:
 //For testing
 int main()
 {
+    //Construct a graph
+    std::ifstream reader("D:\\Harry\\GitHub\\algorithms\\testdata.txt", std::ifstream::in);
+    Graph g;
+    g.readGraph(reader);
+    reader.close();
+    g.printGraph();
 
+    g.BFS(1);
+
+    return 0;
 }
