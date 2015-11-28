0af1fa14c5a14aaaa2f85c2c3219aeccf48eca58
HarryZhang <hnzhang@gmail.com>
2015-11-27 20:56:04 -0800

    workable permutation.cpp

--- permutation.cpp
+++ permutation.cpp
@@ -12,11 +12,11 @@ void backtrack(const std::string& input, std::string output, size_t k, size_t le
     {
         for(size_t i = 0; i < input.size(); i++)
         {
-            output.push_back(input[k]);
+            std::string new_output(output);
+            new_output.push_back(input[i]);
             //sub strin by removing input[i];
             std::string new_input = input.substr(0, i) + input.substr(i+1);
-            std::cout << std::endl << "\t newinput:" << new_input  <<std::endl;
-            backtrack(new_input, output, k+1, length);
+            backtrack(new_input, new_output, k+1, length);
         }
     }
 }
@@ -34,6 +34,8 @@ int main()
 {
     std::string test1("123");
     permutation(test1);
+    std::string test11("1234");
+    permutation(test11);
 
     return 0;
 }
